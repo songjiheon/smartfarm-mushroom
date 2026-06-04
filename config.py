da62fd01bf84fad23bb3e6f8b8704ce47105ec34
songjiheon <syyyh1008@gmail.com>
2026-06-04 15:47:14 +0900

    fix: 값 수정

--- config.py
+++ config.py
@@ -20,21 +20,21 @@ class CO2Config:
 MUSHROOM_PROFILES = {
    "느타리":{
       "발생":{
-         "temp_min":21.0, "temp_max":23.0, "temp_target":22.0,
+         "temp_min":21.0, "temp_max":23.0,"temp_target":22, 
          "humi_min":90.0, "humi_max":95.0,
          "co2_max":1200, "co2_danger":2000,
          "lux_mode":"flash10",
       },
       
       "생육":{
-         "temp_min":16.0, "temp_max":21.0, "temp_target":18.0,
-         "humi_min":75.0, "humi_max":85.0,
+         "temp_min":16.0, "temp_max":21.0,"temp_target":18,
+         "humi_min":85.0, "humi_max":95.0,
          "co2_max": 1500, "co2_danger": 2500,
          "lux_mode":"flash20",
       },
       "수확":{
-         "temp_min": 12.0, "temp_max": 16.0,
-            "humi_min": 60.0, "humi_max": 70.0,
+         "temp_min": 14.0, "temp_max": 16.0,
+            "humi_min": 65.0, "humi_max": 73.0,
             "co2_max": 1100, "co2_danger": 1100,
             "lux_mode": "off",
       }
